0ac0056dc94670648a433de5892e0aabddd963f7
Luiz Ribeiro <luizribeiro@gmail.com>
2011-12-08 00:25:33 -0200

    Trying to get Heroku support

---
+++ src/main/java/org/luizribeiro/gephiviz/Main.java
@@ -0,0 +1,27 @@
+package org.luizribeiro.gephiviz;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.openide.util.Exceptions;
+
+public class Main {
+
+    public static void main(String args[]) {
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        Server server = new Server(Integer.parseInt(webPort));
+
+        WebAppContext webapp = new WebAppContext();
+        webapp.setWar("target/gephiviz.war");
+        server.setHandler(webapp);
+        try {
+            server.start();
+            server.join();
+        } catch (Exception ex) {
+            Exceptions.printStackTrace(ex);
+        }
+    }
+}
