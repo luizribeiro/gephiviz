66743008109f6d5e55bf64dd7ac77bee8e450488
Luiz Ribeiro <luizribeiro@gmail.com>
2012-01-10 22:18:40 -0200

    Removed full page button from the seadragon viewer

--- src/main/webapp/js/gephiviz.js
+++ src/main/webapp/js/gephiviz.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
     var viewer = new Seadragon.Viewer("viewport");
+
+    // remove full page button from the seadragon viewer
+    var navControl = viewer.getNavControl();
+    while (navControl.firstChild !== navControl.lastChild) {
+        navControl.removeChild(navControl.firstChild);
+    }
+
     viewer.openDzi("/tile/666415875/map.xml");
 });
 
